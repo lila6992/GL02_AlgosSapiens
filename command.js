eebacd060333b82249b2a2db0c5f2f527da17cc6
Arthur <arthur.paris@utt.fr>
2024-11-04 11:22:01 +0100

    suppression du tp

--- command.js
+++
@@ -1,9 +0,0 @@
-var Commande = {
-    check: function(){
-    console.log("Commande Ready !")
-    console.log("Magenta level checked".magenta) 
-    console.log("Cyan level checked".cyan)
-    return true
-    }
-    }
-    module.exports = Commande
--- radio.js
+++
@@ -1,9 +0,0 @@
-var radio = {
-    check: function(){
-    console.log("radio Ready !")
-    console.log("Magenta level checked".magenta)
-    console.log("Cyan level checked".cyan)
-    return true
-    }
-}
-module.exports = radio
