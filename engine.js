6b8bbe81478a35ec9050e2b1b626e5362959e4e8
cocochart <corentinchartier334@gmail.com>
2024-11-05 16:50:15 +0100

    Retour du code

---
+++ engine.js
@@ -0,0 +1,9 @@
+var Engine = {
+    check: function(){
+    console.log("Engine Ready !")
+    console.log("Magenta level checked".magenta)
+    console.log("Cyan level checked".cyan)
+    return true
+    }
+    }
+    module.exports = Engine
---
+++ radio.js
@@ -0,0 +1,9 @@
+var radio = {
+    check: function(){
+    console.log("radio Ready !")
+    console.log("Magenta level checked".magenta)
+    console.log("Cyan level checked".cyan)
+    return true
+    }
+}
+module.exports = radio
