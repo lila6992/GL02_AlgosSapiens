258aa11abd0de68d4fa3acda38cb0dc8b76fa846
Jeanne~ <jboyer@dotscreen.com>
2024-12-05 20:29:03 +0100

    fichier examen unhidden

--- Examen.js
+++ Examen.js
@@ -9,18 +9,62 @@ class Examen {
         this.questions = new CollectionQuestion();
     }
     
-    listAllQuestions = function (data, file) {
+    chargeExamQuestions = function (data, file, check) {
         const parser = new GiftParser();
         parser.parse(data, file); 
 
         const parsedQuestions = parser.parse(data, file);
-        parsedQuestions.forEach((question, index) => {
+        if (check == true) {parser.checkFormat(file);}
+        this.questions = parsedQuestions;
+        return parsedQuestions;
+    }
+
+    chargeAllFolderQuestions(dataFolderPath, check) {
+        try {
+            const files = fs.readdirSync(dataFolderPath);
+            const allQuestions = [];
+    
+            // Process each file synchronously
+            files.forEach((file) => {
+                try {
+                    const filePath = path.join(dataFolderPath, file);
+                    const data = fs.readFileSync(filePath, 'utf8'); // Synchronous version of readFile
+                    const fileQuestions = this.chargeExamQuestions(data, file, check); 
+                    allQuestions.push(...fileQuestions); // Add questions to the list
+                } catch (err) {
+                    console.log(`Erreur de lecture du fichier ${file}: ${err.message}`);
+                }
+            });
+    
+            return allQuestions; 
+        } catch (err) {
+            console.log(`Erreur lors du traitement du dossier : ${err.message}`);
+            return []; 
+        }
+    }
+    
+
+    logQuestions = function (questions) {
+        const transformType = (type) => {
+            switch (type) {
+                case 'qcml':
+                    return 'Choix multiples';
+                case 'vrai_faux':
+                    return 'Vrai ou faux';
+                case 'numérique':
+                    return 'Numérique';
+                default:
+                    return type.charAt(0).toUpperCase() + type.slice(1);
+            }
+        };
+
+        questions.forEach((question, index) => {
             // console.log(JSON.stringify(question, null, 2));
             console.log(`\n`);
             console.log(`ID : ${String(question.id)}`);
             console.log(`Fichier source : ${question.file}`);
             console.log(`Titre : ${String(question.title)}`);
-            console.log(`Type : ${question.type}`);
+            console.log(`Type : ${transformType(question.type)}`);
             console.log(`Enoncé : ${question.statement}`);
         
             if (Array.isArray(question.answer) && question.answer.length > 0) {
@@ -39,6 +83,14 @@ class Examen {
         
         });
     }
+
+    search(questions, searchKey) {
+        return questions.filter(q => 
+            (q.title && q.title.toLowerCase().includes(searchKey.toLowerCase())) ||
+            (q.statement && q.statement.toLowerCase().includes(searchKey.toLowerCase()))
+        );
+    }
+
 
     /**
      * Vérifie la validité de l'examen selon les critères définis.
